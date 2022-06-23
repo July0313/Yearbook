d64f897bfd72e9da3d51ce1d481b6d1ca1a5afc0
July0313 <choiheejin5959@gmail.com>
2022-06-23 09:43:57 +0900

    DataManager

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance = null;
+
+    private void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public int coin = 0;
+}
